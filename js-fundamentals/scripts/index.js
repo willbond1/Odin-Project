356742d74ea41656da1f211bcf9403a8a3a42185
Will <willbond1@gmail.com>
2022-06-26 19:22:11 -0400

    Finished JS fundamentals

--- js-fundamentals/scripts/index.js
+++ js-fundamentals/scripts/index.js
@@ -1,4 +1,15 @@
-const string = 'The revolution will not be televised.';
-console.log(string);
-const badString = string;
-console.log(string);
+function add7(n) {
+    return n + 7;
+}
+
+let multiply = (x, y) => x * y;
+
+function capitalize(str) {
+    let firstLetter = str.slice(0, 1).toUpperCase();
+    let theRest = str.slice(1).toLowerCase();
+    return firstLetter + theRest;
+}
+
+let lastletter = (str) => str.slice(-1);
+
+console.log(lastletter("word"));
